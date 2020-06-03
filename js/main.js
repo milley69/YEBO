11b8cc8e6bffeffc6e7f4f39846194dd3ca8551c
milley <53382639+milley69@users.noreply.github.com>
2020-06-03 19:08:28 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
   $('.header__burger').click(function(event) {
     $('nav,.header__burger').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+
+// $(document).ready(function() {
+//   function slowScroll(id) {
+//     $('html, body').animate({
+//       scrollTop: $(id).offset().top
+//     }, 500);
+//   }
+// });
+
+function slowScroll(id) {
+    $('html, body').animate({
+      scrollTop: $(id).offset().top
+    }, 1000);
+    $('nav,.header__burger').removeClass('active');
+}
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    nav: true,
+    loop: true,
+    // margin: 30,
+    smartSpeed: 1000,
+    // freeDrag: true,
+    // mouseDrag: false,
+    autoHeight: true,
+    dots: true,
+    items: 1,
+    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>']
   });
 });
